0fbafa5f4d07b3f80561ab3c870987e8b4f9022d
programando <jhonjamesmg@hotmail.com>
2021-04-22 09:29:26 -0500

    Modify config

--- app/Http/Controllers/Api/FctrasElctrncasInvoicesController.php
+++ app/Http/Controllers/Api/FctrasElctrncasInvoicesController.php
@@ -34,7 +34,7 @@ class FctrasElctrncasInvoicesController
 
 
         public function invoices() {
-            $URL = 'invoice/'. env('FACTURA_ELECT_TEST_ID');
+            $URL = 'invoice' ;
             $Documentos = FctrasElctrnca::InvoicesToSend()->get();  
             
               foreach ($Documentos as $Documento ) {
