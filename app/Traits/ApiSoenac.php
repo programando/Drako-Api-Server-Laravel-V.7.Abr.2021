c0ee6101fdb5878b92bb7042c725cb710a37d982
programando <jhonjamesmg@hotmail.com>
2021-10-25 20:15:11 -0500

    Cambio resolución.

--- app/Traits/ApiSoenac.php
+++ app/Traits/ApiSoenac.php
@@ -29,7 +29,7 @@ trait ApiSoenac {
       public function traitSoenacResolutionsInvoice() {
          $Resolutions =   $this->traitSoenacResolutions();  
          foreach ($Resolutions as $Resolution) {
-            if ( $Resolution['id'] === 7 ){
+            if ( $Resolution['id'] === 10 ){
                return $Resolution;
             }
          }
