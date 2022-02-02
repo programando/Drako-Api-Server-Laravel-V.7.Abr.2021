64f00e866a611937ad60cbb8bc15567648207893
programando <jhonjamesmg@hotmail.com>
2022-02-01 20:28:20 -0500

    seg

--- app/Librarys/GuzzleHttp.php
+++ app/Librarys/GuzzleHttp.php
@@ -34,14 +34,14 @@ class GuzzleHttp {
 
         public function postRequest ( $URL,$Body , $requestNomina=false ){
          $urlRequest    = $requestNomina == false ? config('company.FACTURA_ELECT_URL_BASE')  : config('company.NOMINA_ELECT_URL_BASE');
-          
+          dd( phpinfo());
          $response = $this->Guzzle->request(
             'POST', $URL, [ 
                'headers'  => $this->headers ,
                'json'     => $Body,
                'base_uri' =>  $urlRequest
             ]); 
-            dd( $response );
+            
             return json_decode($response->getBody()->getContents(),true);
     }
 
