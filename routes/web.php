d8155c69a23813d71deab679243e2fd81e82a449
programando <jhonjamesmg@hotmail.com>
2022-02-19 07:45:07 -0500

    Not  own Email

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    //Mail::to("jhonjamesmg@hotmail.com")->send( new TestMail("James"));
+    
     return view('welcome');
     
 });
