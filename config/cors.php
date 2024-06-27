9a14704a31400d9418a9a30e24e492b98477e57b
programando <jhonjamesmg@hotmail.com>
2024-06-27 15:52:37 -0500

    Cors

--- config/cors.php
+++ config/cors.php
@@ -17,7 +17,16 @@ return [
 
     'paths'                    => ['*','/usuarios/login','/usuarios/logout','/usuarios/reset/password','/usuarios/update/password','/sanctum/csrf-cookie', '/usuarios/autenticado','/usuarios/buscar/email'],
     'allowed_methods'          => ['*'],
-    'allowed_origins'          => ['http://localhost:3000','http://localhost:3001','https://drako.facturas.plusoft.co', 'https://api.drako.com.co','http://localhost:8000','https://facturas.drako.com.co', 'https://drako.com.co'],
+    'allowed_origins'          => [
+                    'http://localhost:3000',
+                    'http://localhost:3001',
+                    'http://localhost:8000',
+                    'https://api.drako.com.co',
+                    'https://drako.com.co', 
+                    'https://drako.facturas.plusoft.co', 
+                    'https://facturas.drako.com.co', 
+                    'https://pos.drako.com.co/'
+                ],
     'allowed_origins_patterns' => [],
     'allowed_headers'          => ['*'],
     'exposed_headers'          => false,
